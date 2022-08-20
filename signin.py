5fcd77858d132915921ef88aaddd930f3ecc0f51
liadk07 <91607695+liad07@users.noreply.github.com>
2022-08-21 01:41:41 +0300

    Update signin.py

--- signin.py
+++ signin.py
@@ -9,18 +9,6 @@ users=open("users.txt","a",encoding="utf8")
 def add(user,password,mail,fname,lname):
     users.write(f"{user},{password},{mail},{fname},{lname}\n")
     return ""
-@app.route('/signin/<user>/<mail>/<password>/')
-def add1(user,password,mail):
-    #users=""
-    users = open("users.txt", "r", encoding="utf8")
-    arr=users.read().split("\n")
-    #print(users.read())
-    ret=""
-    for i in range (len(arr)-1):
-        print(f"{user},{mail},{password}" , arr[i])
-        if f"{user},{mail},{password}" in arr[i]:
-            ret=arr[i]
-    return f"{ret}"
 
 app.run()
 users.close()
